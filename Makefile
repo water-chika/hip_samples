55464faa08f00d328def0994166aefb22176b3a6
Water Chika <water_chika@outlook.com>
2025-08-31 23:36:51 +0000

    Update build system

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ all: hip_samples_build
 	./hip_samples_build
 
 hip_samples_build: build.cpp build/build.hpp
-	c++ build.cpp -o hip_samples_build -std=c++20 -ltbb
+	c++ build.cpp -o hip_samples_build -std=c++23 -ltbb
 
 load_store: load_store.cpp Makefile
 	amdclang++ --offload-arch=gfx1201 -x hip load_store.cpp -I /opt/rocm/include -std=c++20 -o load_store --save-temps -g
--- build.cpp
+++ build.cpp
@@ -1,45 +1,80 @@
 #include "build/build.hpp"
 #include <iostream>
+#include <format>
+#include <ranges>
+
+auto c_plus_plus_compile(const std::vector<std::string>& src, const std::string& output_path) {
+    std::string sources = "";
+    std::ranges::for_each(
+            src,
+            [&sources](auto s) {
+                sources += " " + s;
+            }
+            );
+    auto command = std::format("c++ {} -o {} -std=c++23 -ltbb", sources, output_path);
+    std::cout << command << std::endl;
+    return system(command.c_str());
+}
+auto c_plus_plus_compile(const std::string src, const std::string& output_path) {
+    return c_plus_plus_compile(std::vector<std::string>{src}, output_path);
+}
+auto amdclang_plus_plus_compile(const std::vector<std::string>& src, const std::string& output_path) {
+    auto command = std::format("amdclang++ -x hip --offload-arch=gfx1201 {} -o {} -std=c++20", src, output_path);
+    std::cout << command << std::endl;
+    return system(command.c_str());
+}
 
 int main() {
-    auto build_hpp =
-        std::make_shared<build::file_object>(
-                "build/build.hpp"
-        );
-    auto build_cpp = std::make_shared<build::file_object>("build.cpp");
+    auto build_hpp = build::file("build/build.hpp");
+    auto build_cpp = build::file("build.cpp");
     
-    auto hip_samples_build = std::make_shared<build::file_object>(
-            "hip_samples_build",
-            std::vector<std::weak_ptr<build::file_object>>{build_hpp, build_cpp},
-            [](){
-                std::cout << "building" << std::endl;
-                auto command = "c++ build.cpp -o hip_samples_build -std=c++20 -ltbb";
-                system(command);
-                std::cout << command << std::endl;
+    auto hip_samples_build = build::file("hip_samples_build", build::dependencies(build_hpp, build_cpp),
+            [](const auto& target){
+                auto weak_sources = target.get_dependencies();
+                auto sources = std::vector<std::shared_ptr<build::file_object>>(weak_sources.size());
+                std::ranges::transform(
+                        weak_sources,
+                        sources.begin(),
+                        [](auto e) {
+                            return e.lock();
+                        }
+                        );
+                auto sources_string = std::vector<std::string>(sources.size());
+                std::ranges::transform(
+                        sources,
+                        sources_string.begin(),
+                        [](auto& src) {
+                            return src->get_file_path().string();
+                        }
+                        );
+                auto res = c_plus_plus_compile(sources_string, target.get_file_path());
+                return res == 0 ? build::update_res::success : build::update_res::failed;
             }
             );
 
-    auto load_store_cpp = std::make_shared<build::file_object>("load_store.cpp");
-    auto load_store = std::make_shared<build::file_object>(
+    auto load_store_cpp = build::file("load_store.cpp");
+    auto load_store = build::file(
             "load_store",
-            std::vector<std::weak_ptr<build::file_object>>{load_store_cpp},
-            []() {
+            build::dependencies(load_store_cpp),
+            [](auto) {
                 std::cout << "building" << std::endl;
                 auto command = "amdclang++ -x hip --offload-arch=gfx1201 load_store.cpp -o load_store -std=c++20";
-                system(command);
+                auto res = system(command);
                 std::cout << command << std::endl;
+                return res == 0 ? build::update_res::success : build::update_res::failed;
             }
             );
 
-    auto dump_register_cpp = std::make_shared<build::file_object>("dump_register.cpp");
-    auto dump_register = std::make_shared<build::file_object>(
+    auto dump_register_cpp = build::file("dump_register.cpp");
+    auto dump_register = build::file(
             "dump_register",
-            std::vector<std::weak_ptr<build::file_object>>{dump_register_cpp},
-            []() {
+            build::dependencies(dump_register_cpp),
+            [](auto) {
                 std::cout << "building" << std::endl;
                 auto command = "amdclang++ -x hip --offload-arch=gfx1201 dump_register.cpp -o dump_register -std=c++20";
-                system(command);
+                auto res = system(command);
                 std::cout << command << std::endl;
+                return res == 0 ? build::update_res::success : build::update_res::failed;
             }
             );
 
